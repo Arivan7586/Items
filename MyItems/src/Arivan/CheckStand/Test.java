467888132cb1598e5790efd2b3ae5419b2866a83
Arivan7586 <1194028288@qq.com>
2019-04-25 09:30:37 +0800

    简易收银台（未完成）-完善订单管理

--- MyItems/src/Arivan/CheckStand/Test.java
+++ MyItems/src/Arivan/CheckStand/Test.java
@@ -1,17 +1,27 @@
 package Arivan.CheckStand;
 
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
         Client.run();
 
-        StringBuilder stringBuilder = new StringBuilder("abfdfsf");
-        while (stringBuilder.length() < 10) {
-            stringBuilder.append(1);
-        }
-        System.out.println(stringBuilder);
+//        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(format.format(new Date()));
+
+//        String s = "0.5";
+//        String m = "0.5.6";
+//        System.out.println(GoodsManage.isAllLongDigit(s));
+//        System.out.println(GoodsManage.isAllLongDigit(m));
+//        System.out.println(GoodsManage.isAllDigit(s));
+
+//        StringBuilder stringBuilder = new StringBuilder("abfdfsf");
+//        while (stringBuilder.length() < 10) {
+//            stringBuilder.append(1);
+//        }
+//        System.out.println(stringBuilder);
 
 //        Scanner scanner = new Scanner(System.in);
 //        String s = scanner.nextLine();
@@ -66,6 +76,9 @@ class Surface {
     }
 }
 
+/**
+ * 客户端
+ */
 class Client {
     public static void run() {
         Scanner scanner = new Scanner(System.in);
@@ -74,7 +87,7 @@ class Client {
             Surface.mainMenu();
             switch (scanner.nextLine()) {
                 case "U":
-                    System.out.println("111111111111111");
+                    OrderManage.buyMenu();
                     break;
                 case "S":
                     GoodsManage.set();
@@ -89,18 +102,22 @@ class Client {
                     Surface.exitMenu();
                     flag = false;
                     break;
-                    default:
-                        System.out.println("指令输入错误！请重新输入！");
-                        break;
+                default:
+                    System.out.println("指令输入错误！请重新输入！");
+                    break;
             }
         }
     }
 
 }
 
+/**
+ * 商品管理
+ */
 class GoodsManage {
-    public static final TreeMap<Integer,String[]> goods =
+    public static final TreeMap<Integer, String[]> goods =
             new TreeMap<>();
+
     public static void set() {
         Scanner scanner = new Scanner(System.in);
         Surface.setMenu();
@@ -206,14 +223,14 @@ class GoodsManage {
             String[] arr = enter.trim().split(" ");
             if (arr.length != 3) {
                 System.out.println("输入有误，参数个数不等于3！");
-            }else if (!isAllDigit(arr[0])) {
+            } else if (!isAllDigit(arr[0])) {
                 System.out.println("输入有误，商品编号不是纯数字！");
-            }else if (isAllLongDigit(arr[2])) {
+            } else if (!isAllLongDigit(arr[2])) {
                 System.out.println("输入有误，商品价格不是含小数点的数字！");
-            }else {
+            } else {
                 int id = Integer.parseInt(arr[0]);
-                String[] value = new String[]{arr[1],arr[2]};
-                goods.put(id,value);
+                String[] value = new String[]{arr[1], arr[2]};
+                goods.put(id, value);
                 break;
             }
         }
@@ -245,10 +262,11 @@ class GoodsManage {
 
     /**
      * 判断一个字符串是否全由数字组成
+     *
      * @param str 要判断的字符串
      * @return
      */
-    private static boolean isAllDigit(String str) {
+    public static boolean isAllDigit(String str) {
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) > 57 || str.charAt(i) < 48) {
                 return false;
@@ -259,14 +277,160 @@ class GoodsManage {
 
     /**
      * 判断字符串是否是带有小数点的数字
+     *
      * @param str 需要判断的字符串
      * @return 是，返回true，否则返回false
      */
-    private static boolean isAllLongDigit(String str) {
-        if (!str.contains(".")) {
-            return false;
+    public static boolean isAllLongDigit(String str) {
+        int count = 1;
+        int i = 0;
+        while (count >= 0 && i < str.length()) {
+            if (str.charAt(i) == '.' && i == 0) {
+                return false;
+            }else if (str.charAt(i) == '.') {
+                count--;
+            }else if (str.charAt(i) > 57 || str.charAt(i) < 48) {
+                return false;
+            }
+            i++;
         }
+        if (count == 0) {
+            return true;
+        }
+        return false;
+    }
+}
 
-        return isAllDigit(str.replace("","\\."));
+/**
+ * 订单管理
+ */
+class OrderManage {
+    private static Integer T = 1;
+    public static final TreeMap<Integer,String[]> orders =
+            new TreeMap<>();
+    public static void buyMenu() {
+//        GoodsManage.view();
+        System.out.println("******************* 买单功能 ********************");
+        System.out.println("    [S] 查看 [A] 下单 [D] 取消 [L] 浏览 [R] 返回");
+        System.out.println("       输入:  S A D L R 进入操作");
+        System.out.println("************************************************");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("R")) {
+                break;
+            }else if (input.equals("S") || input.equals("A") || input.equals("D")
+            || input.equals("L")) {
+                switch (input) {
+                    case "S":
+                        orderView();
+                        while (true) {
+                            System.out.println("返回请输入R");
+                            if (scanner.nextLine().equals("R")) {
+                                break;
+                            }
+                        }
+                        buyMenu();
+                        break;
+                    case "A":
+                        if (GoodsManage.goods.isEmpty()) {
+                            System.out.println("当前货物为空！！！");
+                        }else {
+                            buy();
+                        }
+                        buyMenu();
+                        break;
+                    case "D":
+                        System.out.println("取消订单");
+                        break;
+                    case "L":
+                        browse();
+                        while (true) {
+                            System.out.println("返回请输入R");
+                            if (scanner.nextLine().equals("R")) {
+                                break;
+                            }
+                        }
+                        buyMenu();
+                        break;
+                    default:
+                            break;
+                }
+            }else {
+                System.out.println("指令输入错误，请重新输入");
+            }
+
+        }
+    }
+
+    /**
+     * 下单
+     */
+    public static void buy() {
+        System.out.println("请输入下单信息[编号 数量]（如下格式：1  2 ）:");
+        System.out.println("放弃操作，请输入R");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String intput = scanner.nextLine();
+            if (intput.equals("R")) {
+                break;
+            }
+            String[] arr = intput.trim().split(" ");
+            if (arr.length != 2) {
+                System.out.println("输入有误，参数个数不等于3！");
+            } else if (!GoodsManage.isAllDigit(arr[0])) {
+                System.out.println("输入有误，商品编号不是纯数字！");
+            } else if (!GoodsManage.isAllDigit(arr[1])) {
+                System.out.println("输入有误，商品编号不是纯数字！");
+            } else {
+                int id = Integer.parseInt(arr[0]);
+                String count = arr[1];
+                String[] good = GoodsManage.goods.get(id);
+                String[] orser = new String[]{good[0],count,good[1]};
+                orders.put(id,orser);
+                orderView();
+            }
+        }
+    }
+
+    /**
+     * 查看订单信息
+     */
+    public static void orderView() {
+        Date date = new Date();
+        System.out.println("===============================");
+        System.out.println("编号："+T++);
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("打印时间："+format.format(new Date()));
+        System.out.println("===============================");
+        System.out.println("编号     名称      数量     单价");
+        System.out.println("===============================");
+        System.out.println("总价："+ ordersSum());
+        System.out.println("===============================");
+    }
+
+    private static double ordersSum() {
+        Iterator<Integer> iterator = orders.keySet().iterator();
+        double sum = 0;
+        while (iterator.hasNext()) {
+            String[] value = orders.get(iterator.next());
+            sum += (Integer.parseInt(value[1])*Integer.parseInt(value[2]));
+        }
+        return sum;
+    }
+
+    /**
+     * 浏览商品
+     */
+    public static void browse() {
+        GoodsManage.view();
+    }
+
+    /**
+     * 取消
+     */
+    public static void cancel() {
+
     }
 }
